0c823149fcab58625ebe17568e52205acc27d8ce
chibimagic <github@chibimagic.org>
2015-07-13 11:56:20 -0700

    Xcode default project generation

---
+++ Montana/Montana/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Montana
+//
+
+//  Copyright (c) 2015 chibimagic. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Montana/Montana/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Montana
+//
+//  Created by Eileen Xie on 7/13/15.
+//  Copyright (c) 2015 chibimagic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
