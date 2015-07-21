a4169b9d619c25e6ecb12d46ab300982a135df83
chibimagic <github@chibimagic.org>
2015-07-20 21:48:06 -0700

    HighlightNode: Class to highlight a position on the board

---
+++ Montana/HighlightNode.h
@@ -0,0 +1,15 @@
+//
+//  HighlightNode.h
+//  Montana
+//
+//  Created by Eileen Xie on 7/20/15.
+//  Copyright (c) 2015 chibimagic. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface HighlightNode : SKShapeNode
+
+- (instancetype)initWithSize:(CGSize)size;
+
+@end
---
+++ Montana/HighlightNode.m
@@ -0,0 +1,35 @@
+//
+//  HighlightNode.m
+//  Montana
+//
+//  Created by Eileen Xie on 7/20/15.
+//  Copyright (c) 2015 chibimagic. All rights reserved.
+//
+
+#import "HighlightNode.h"
+
+@implementation HighlightNode
+
++ (CGPathRef)pathForSize:(CGSize)size {
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    [path moveToPoint:CGPointMake(0, size.height)];
+    [path addLineToPoint:CGPointMake(size.width, size.height)];
+    [path addLineToPoint:CGPointMake(size.width, 0)];
+    [path addLineToPoint:CGPointMake(0, 0)];
+    [path addLineToPoint:CGPointMake(0, size.height)];
+    return [path CGPath];
+}
+
+- (instancetype)initWithSize:(CGSize)size {
+    self = [super init];
+    if (self) {
+        CGPathRef path = [[self class] pathForSize:size];
+        [self setPath:path];
+        [self setFillColor:[UIColor yellowColor]];
+        [self setAlpha:0.5];
+        [self setZPosition:1];
+    }
+    return self;
+}
+
+@end
